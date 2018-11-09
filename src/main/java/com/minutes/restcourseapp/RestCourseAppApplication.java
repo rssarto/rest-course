c82cc9867c63e93e49d61dd7f26532eae4d6c4dc
rssarto <ricardo.soares.sarto@gmail.com>
2018-11-09 14:52:35 -0200

    swagger documentation

--- src/main/java/com/minutes/restcourseapp/RestCourseAppApplication.java
+++ src/main/java/com/minutes/restcourseapp/RestCourseAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -18,15 +19,17 @@ public class RestCourseAppApplication {
 	
 	@Bean
 	public LocaleResolver localResolver() {
-		SessionLocaleResolver sessionLocalResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver sessionLocalResolver = new AcceptHeaderLocaleResolver();
 		sessionLocalResolver.setDefaultLocale(Locale.US);
 		return sessionLocalResolver;
 	}
 	
+	/*
 	@Bean
 	public ResourceBundleMessageSource resourceBundleMessageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("messages");
 		return messageSource;
 	}
+	*/
 }
--- src/main/java/com/minutes/restcourseapp/user/UserController.java
+++ src/main/java/com/minutes/restcourseapp/user/UserController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -18,26 +20,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 public class UserController {
 	
 	@Autowired
 	private UserDAO userDAO;
 	
+	@Autowired
+	ResourceBundleMessageSource resourceBundleMessageSource;
+	
 	@GetMapping("/users")
+	@ApiOperation(value="Retrieve all users recorded in database.")
 	public List<User> getAll(){
 		List<User> allUsers = this.userDAO.getAll();
 		if( allUsers == null || allUsers.isEmpty() ) {
-			throw new UserNotFoundException("no uset found");
+			throw new UserNotFoundException(this.resourceBundleMessageSource.getMessage("exception.notfound", null, LocaleContextHolder.getLocale()));
 		}
 		return allUsers;
 	}
 	
 	@GetMapping("/users/{id}")
-	public Resource<User> get(@PathVariable int id) {
+	@ApiOperation(value="Retrieve a specific user")
+	public Resource<User> get(@ApiParam(value="User's id to be retrieved.") @PathVariable int id) {
 		User user = this.userDAO.get(id);
 		if( user == null ) {
-			throw new UserNotFoundException("id: " + id);
+			throw new UserNotFoundException(this.resourceBundleMessageSource.getMessage("exception.notfound", null, LocaleContextHolder.getLocale()) + " id: " + id);
 		}
 		
 		//HATEOAS - Hypermedia as the engine of application state
@@ -48,7 +58,8 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<User> save(@Valid @RequestBody User user) {
+	@ApiOperation(value="Creates or Updates a user")
+	public ResponseEntity<User> save(@ApiParam(value="User definition that is going to be created.") @Valid @RequestBody User user) {
 		if( user.getId() != null ) {
 			User savedUser = this.userDAO.get(user.getId());
 			if( savedUser == null ) {
@@ -66,7 +77,8 @@ public class UserController {
 	}
 	
 	@DeleteMapping("/users/{id}")
-	public void delete(@PathVariable Integer id) {
+	@ApiOperation(value="Deletes a user by id.")
+	public void delete(@ApiParam(value="Users id that is going to be deleted.") @PathVariable Integer id) {
 		User deletedUser = this.userDAO.delete(id);
 		if( deletedUser == null ) {
 			throw new UserNotFoundException("id: " + id);
