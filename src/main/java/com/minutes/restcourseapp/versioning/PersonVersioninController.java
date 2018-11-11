d18a52758a340afac6ccd17b273eabeb73cd8681
rssarto <ricardo.soares.sarto@gmail.com>
2018-11-11 13:36:52 -0200

    versioning lesson

--- src/main/java/com/minutes/restcourseapp/versioning/PersonVersioninController.java
+++ src/main/java/com/minutes/restcourseapp/versioning/PersonVersioninController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioninController {
 	
+	/* URL PARAMETER VERSIONING */
 	@GetMapping("/v1/person")
 	public PersonV1 getPersonV1() {
 		return new PersonV1("Ricardo");
@@ -16,4 +17,37 @@ public class PersonVersioninController {
 		return new PersonV2(new Name("Ricardo", "Sarto"));
 	}
 
+	/* REQUEST PARAMETER VERSIONING */
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 getPersonParamV1() {
+		return new PersonV1("Ricardo");
+	}
+	
+	@GetMapping(value="/person/param", params="version=2")
+	public PersonV2 getPersonParamV2() {
+		return new PersonV2(new Name("Ricardo", "Sarto"));
+	}
+	
+	/* HEADER PROPERTY VERSIONING */
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 getPersonHeaderV1() {
+		return new PersonV1("Ricardo");
+	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 getPersonHeaderV2() {
+		return new PersonV2(new Name("Ricardo", "Sarto"));
+	}
+	
+	/* HEADER PRODUCES PROPERY VERSIONING */
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 getPersonProducesV1() {
+		return new PersonV1("Ricardo");
+	}
+	
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 getPersonProducesV2() {
+		return new PersonV2(new Name("Ricardo", "Sarto"));
+	}
+	
 }
